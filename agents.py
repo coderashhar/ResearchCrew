e4739de2782e117e31ba222bb5800460a38d4d9a
Mohd. Ashhar Khan <ashhar2003.khan@gmail.com>
2026-06-24 13:51:21 +0530

    added web search agent

---
+++ agents.py
@@ -0,0 +1,18 @@
+from langchain.agents import create_agent
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from tools import web_search, scrape_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# model setup
+llm = ChatMistralAI(model="mistral-medium-3-5", temperature=0)
+
+# search agent 
+def build_search_agent():
+    return create_agent(
+        model = llm,
+        tools = [web_search],
+    )
