acbe39c40e1cab89d9158eb90e94bcded1a179e4
Mohd. Ashhar Khan <ashhar2003.khan@gmail.com>
2026-06-24 13:29:47 +0530

    added web search tool

---
+++ tools.py
@@ -0,0 +1,21 @@
+import os  
+from dotenv import load_dotenv
+load_dotenv()
+from langchain.tools import tool
+import requests
+from bs4 import BeautifulSoup
+from tavily import TavilyClient
+from rich import print
+
+tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+@tool
+def web_search(query: str) -> str:
+    """Search the web for recent and reliable information on a topic. Returns Titles, URLs and Snippets."""
+    results = tavily.search(query=query, max_results=5)
+    output = []
+    for r in results['results']:
+        output.append(f"Title: {r['title']}\n URL: {r['url']}\n snippet:{r['content'[:300]]}\n")
+    return "\n--------------------------------------------------------------------------\n".join(output)
+print(web_search.invoke("What is the latest news on AI?"))
+
